27b0ac067214585cbdf0de97252102eee057806a
voanhtuan123 <106895859+voanhtuan123@users.noreply.github.com>
2024-10-17 19:08:50 -0600

    Add files via upload

---
+++ Assignment2_draft2.py
@@ -0,0 +1,44 @@
+list_num = 0
+while list_num < 2:
+    list_num = int(input("How many data points do you have? "))
+    if list_num < 2:
+        print("Must enter at least two data points.")
+
+x = 1
+years = []
+fertility = []
+while x <= list_num:
+        print("What is the year of Datapoint",x,"?" )
+        year = int(input())
+        print("What is the fertility rate of Datapoint",x,"?")
+        fertility_rate = float(input())
+
+        if year in years:
+            fertility_rate = years.index(year)
+
+        else:
+            years.append(year)
+            fertility.append(fertility_rate)
+            x = x + 1
+print(years)
+print(fertility)
+            
+start_year = int(input("Which year you would like to start with?"))
+if start_year in years:
+    end_year = int(input("Which year you would like to end with?"))
+    if end_year in years:
+        if end_year > start_year:
+            ave = (fertility[years.index(start_year)] + fertility[years.index(end_year)])/2
+            print(f"The average fertility rate of these two years is {ave:.2f}.")
+            if fertility[years.index(start_year)] > fertility[years.index(end_year)]:
+                print("There is a downward trend.")
+            elif fertility[years.index(start_year)] < fertility[years.index(end_year)]:
+                print("There is an upward trend.")
+            else:
+                print("There is a sideways trend.")
+        else:
+            print("End year must be after start year.")
+    else:
+        print("The end year does not exist.")
+else:
+    print("The start year does not exist.")
