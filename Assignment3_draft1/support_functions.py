5a88cae44106397e31935de99052563923d72b83
voanhtuan123 <voanhtuantia@gmail.com>
2024-11-10 18:14:24 -0700

    draft 2 assignmentt

--- Assignment3_draft1/support_functions.py
+++ Assignment3_draft1/support_functions.py
@@ -7,40 +7,28 @@ def get_greetings(name): #function1
 
 def full_word(sentence,word):     #function2
 
-    ans = False
     # Convert both sentence and word to lowercase for case insensitivity and add " " to start and end of sentence
     SentenceLowercase = sentence.lower()
     WordLowercase = word.lower()
     SentenceLowercase = " " + SentenceLowercase + " "
+        # List of separators
+    separators = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "{", "|", "}", "~", "`"]
     # if equal then return right away
     if SentenceLowercase == WordLowercase:
         return True
-    WordLen = len(WordLowercase)
-    SentLen = len(SentenceLowercase)
 
-    # List of separators
-    separators = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "{", "|", "}", "~", "`"]
-    
-
-    for i in range(1,SentLen):
-        # Keep track of the char in sentence
-        track = 0
+    for i in range(len(SentenceLowercase)):
         # If a char in sentence equals the first char in word and the previous char in sentence must be a separator
         if SentenceLowercase[i] == WordLowercase[0] and SentenceLowercase[i-1] in separators:
-            # Checking for the index range
-            if SentLen - i < WordLen:
-                continue
-            else:
-                for j in range(WordLen):
+                for x in range(len(WordLowercase)):
                     # Checking if words by words is equal
-                    if WordLowercase[j] == SentenceLowercase[j + i]:
-                        track = j + i
+                    if WordLowercase[x] == SentenceLowercase[x + i]:
                         ans = True
                     else:
-                        print(ans)
+                        print(False)
                         continue
                 # Checking for validity and if the next char is a separator
-                if SentenceLowercase[track + 1] in separators and ans == True:
+                if SentenceLowercase[x + i + 1] in separators and ans == True:
                     return True
     return False
 
@@ -55,3 +43,4 @@ def get_answer(database, sentence):
         return "Sorry, I do not understand your question."
     
 
+print(full_word("University of Calgary","uni"))
